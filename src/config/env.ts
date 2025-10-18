28a4da420223839d1b658ca3154281a07f1fc8b5
RoberthLoorDev <roberthloor.dev@gmail.com>
2025-10-18 08:10:35 -0500

    add: actualizar configuración de variables de entorno en .env.example y env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,14 +1,30 @@
 import dotenvSafe from "dotenv-safe";
 import path from "path";
 
-// Load environment variables from .env file
 dotenvSafe.config({
      example: path.resolve(__dirname, "../../.env.example"),
 });
 
-//export variables
+const {
+     PORT = "3000",
+     GEMINI_API_KEY,
+     DATABASE_URL,
+     POSTGRES_USER,
+     POSTGRES_PASSWORD,
+     POSTGRES_DB,
+     POSTGRES_PORT = "5433",
+     DB_HOST = "localhost",
+     NODE_ENV,
+} = process.env;
+
 export const ENV = {
-     PORT: parseInt(process.env.PORT || "3000", 10),
-     DATABASE_URL: process.env.DATABASE_URL as string,
-     GEMINI_API_KEY: process.env.GEMINI_API_KEY as string,
+     PORT: parseInt(PORT, 10),
+     GEMINI_API_KEY: GEMINI_API_KEY as string,
+     POSTGRES_USER: POSTGRES_USER as string,
+     POSTGRES_PASSWORD: POSTGRES_PASSWORD as string,
+     POSTGRES_DB: POSTGRES_DB as string,
+     POSTGRES_PORT: parseInt(POSTGRES_PORT, 10),
+     DB_HOST,
+     DATABASE_URL: DATABASE_URL as string,
+     NODE_ENV: NODE_ENV || "development",
 };
