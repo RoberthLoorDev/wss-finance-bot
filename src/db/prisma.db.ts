db3ed374f0aa0b1b2bb27e2286b78ce40c7ff457
RoberthLoorDev <roberthloor.dev@gmail.com>
2025-10-24 16:49:12 -0500

    fix: ajustar niveles de log para el cliente Prisma en desarrollo

--- src/db/prisma.db.ts
+++ src/db/prisma.db.ts
@@ -3,7 +3,7 @@ import { ENV } from "../config/env";
 
 export const prisma = new PrismaClient({
      datasources: { db: { url: ENV.DATABASE_URL } },
-     log: ENV.NODE_ENV === "development" ? ["query", "info", "warn", "error"] : ["warn", "error"], // Log all queries in development, only warnings and errors in production
+     log: ENV.NODE_ENV === "development" ? ["info", "warn", "error"] : ["warn", "error"], // Log all queries in development, only warnings and errors in production
 });
 
 // Disconnect Prisma Client when the Node.js process ends
