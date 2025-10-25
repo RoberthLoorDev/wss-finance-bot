1f8e8fbb5044f7ca5d3b4ff0b1570fbd7e2eb897
RoberthLoorDev <roberthloor.dev@gmail.com>
2025-10-25 09:37:06 -0500

    add: crear tablas Conversation y Message, y agregar columna telegram_user_id a User

---
+++ prisma/migrations/20251025140210_add_conversations_and_messages_to_model_database/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Conversation" (
+    "id" BIGSERIAL NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "title" TEXT,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "started_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ended_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" BIGSERIAL NOT NULL,
+    "conversation_id" BIGINT NOT NULL,
+    "sender" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Conversation" ADD CONSTRAINT "Conversation_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_conversation_id_fkey" FOREIGN KEY ("conversation_id") REFERENCES "Conversation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
